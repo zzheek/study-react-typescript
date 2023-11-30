8a9482f9ac5a4975228e5e9887b04a42e456151f
강주희 <133830185+zzheek@users.noreply.github.com>
2023-11-30 13:58:38 +0900

    Create State.tsx

---
+++ State.tsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  `;
+
+const spinAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${spinAnimation} infinite 20s linear;
+  }
+`;
+
+const AppLink = styled.a`
+  color: #61afb;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 32px;
+`;
+const Contents = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Lable = styled.span`
+  margin: 0 16px;
+  font-size: 1.2rem;
+`;
+const Button = styled.button`
+  border: 0;
+  color: #ffffff;
+  background-color: #ff5722;
+  cursor: pointer;
+  padding: 8px 16px;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #ff5722;
+    opacity: 0.8;
+  }
+
+  &:active {
+    box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  const sub = () => {
+    setCounter(counter - 1);
+  };
+  const add = () => {
+    setCounter(counter + 1);
+  };
+
+  return (
+    <Container>
+      <Title>Counter App</Title>
+      <Contents>
+        <Button onClick={sub}>-</Button>
+        <Lable>{counter}</Lable>
+        <Button onClick={add}>+</Button>
+      </Contents>
+  </Container>
+  );
+}
+
+export default App;
